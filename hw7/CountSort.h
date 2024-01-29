7280aa620d06b4139e62784639ad2176caad4873
Daniel Garon <dgaron@colostate.edu>
2024-01-29 11:02:06 -0700

    Removes comment

--- hw7/CountSort.h
+++ hw7/CountSort.h
@@ -15,7 +15,7 @@ class CountSort {
   public:
     class Iterator {
       private:
-        const CountSort &parent; // Pointer to a const CountSort not a const pointer
+        const CountSort &parent;
         int array_index;
         int element_index = 1;
       public:
